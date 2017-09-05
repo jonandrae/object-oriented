6b01cc59048488326859878432feb018a2de8940
jonandrae <31227066+jonandrae@users.noreply.github.com>
2017-09-05 05:59:51 -0500

    Create IAccount.cs

---
+++ IAccount.cs
@@ -0,0 +1,10 @@
+using System;
+
+public Interface IAccount{
+
+//Account number for various account types
+String AccountNumber;
+
+//method to retreive the account number
+public String getAccountNumber(); 
+}
